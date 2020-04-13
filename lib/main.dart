d9e5c6f69304c213ac1ac88ecec856e435482346
gino abella <gino@ginos-MacBook-Air.local>
2020-04-13 12:03:10 +0800

    end of module 44

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,54 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_complete_guide/question.dart';
+import 'package:flutter_complete_guide/answer.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      {
+        'questionText': 'What\'s your favorite color',
+        'answers': ['Black', 'Red', 'Green', 'White']
+      },
+      {
+        'questionText': 'What\'s your favorite animal',
+        'answers': ['Dog', 'Cat', 'Horse', 'Tiger']
+      },
+      {
+        'questionText': 'What\'s your favorite car',
+        'answers': ['Vios', 'Avanza', 'Innova', 'Fortuner']
+      },
+    ];
     return MaterialApp(
-      home: Text('Hello There'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Question(questions[_questionIndex]['questionText']),
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) => Answer(_answerQuestion, answer))
+                .toList()
+          ],
+        ),
+      ),
     );
   }
 }
